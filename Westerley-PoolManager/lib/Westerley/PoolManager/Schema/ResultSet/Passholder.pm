530971bac5072421efcba6eb526bde6ab23991dc
Anthony DeRobertis <anthony@derobert.net>
2014-04-11 03:15:40 -0400

    Other docs said to use this magic boilerplate.

--- Westerley-PoolManager/lib/Westerley/PoolManager/Schema/ResultSet/Passholder.pm
+++ Westerley-PoolManager/lib/Westerley/PoolManager/Schema/ResultSet/Passholder.pm
@@ -2,10 +2,13 @@ use utf8;
 package Westerley::PoolManager::Schema::ResultSet::Passholder;
 
 use Moose;
+use namespace::autoclean;
 use MooseX::NonMoose;
-use MooseX::MarkAsMethods autoclean => 1;
 
 extends 'DBIx::Class::ResultSet';
-__PACKAGE__->load_components('Helper::ResultSet::AutoRemoveColumns');
 
+sub BUILDARGS { $_[2] }
+
+__PACKAGE__->load_components('Helper::ResultSet::AutoRemoveColumns');
+__PACKAGE__->meta->make_immutable;
 1;
