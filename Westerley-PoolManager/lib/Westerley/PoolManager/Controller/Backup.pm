fae9d34390eb7504ab0f63eaaa26324a3e6e6ce0
Anthony DeRobertis <anthony@derobert.net>
2014-08-03 01:31:52 -0400

    Now can do a visitor log export.
    This required some changes and cleanups to the backup interface. For
    example, its of course required to select which operation you want.
    
    Also, some minor refactoring.

--- Westerley-PoolManager/lib/Westerley/PoolManager/Controller/Backup.pm
+++ Westerley-PoolManager/lib/Westerley/PoolManager/Controller/Backup.pm
@@ -4,6 +4,7 @@ use Moose::Util::TypeConstraints;
 use namespace::autoclean;
 use autodie qw(:all);
 use DateTime;    # massive overkill, but avoid POSIX::strftime (Windows)
+use Text::CSV;
 
 BEGIN { extends 'Catalyst::Controller'; }
 
@@ -23,11 +24,18 @@ has db_name => (
 	required => 1
 );
 
-has destination => (
+has backup_relpath => (
 	is       => 'ro',
 	isa      => 'Str',
 	required => 1,
-	default  => '/media/backup/pool-%Y-%m-%d_%H%M%S_%Z.pg'
+	default  => 'backup-%Y-%m-%d_%H%M%S_%Z.pg'
+);
+
+has export_relpath => (
+	is       => 'ro',
+	isa      => 'Str',
+	required => 1,
+	default  => 'export-%Y-%m-%d_%H%M%S_%Z.csv'
 );
 
 has compression => (
@@ -64,14 +72,32 @@ sub index :Path :Args(0) {
 sub run :Local :Args(0) {
 	my ($self, $c) = @_;
 	my $device = $c->req->params->{destination};
+	my $op = $c->req->params->{op};
 
 	if (($device // '') eq '') {
 		$c->stash(err_no_device => 1);
 		$c->detach;
 	}
 
+	if (($op // '') eq '') {
+		$c->stash(err_no_op => 1);
+	} elsif ('backup' eq $op) {
+		$c->detach(op_backup => [ $device ]);
+	} elsif ('log_exp' eq $op) {
+		$c->detach(op_log_export => [ $device ]);
+	} else {
+		$c->stash(err_bad_op => $op);
+		$c->detach;
+	}
+
+}
+
+sub op_backup : Private {
+	my ($self, $c, $device) = @_;
+	$c->stash(template => 'backup/op_backup.tt2');
+
 	my $now = DateTime->now;
-	my $filename = $now->strftime($self->destination);
+	my $filename = $now->strftime($self->backup_relpath);
 	$c->stash(file => $filename);
 
 	$c->log->info("Requesting mount of $device...");
@@ -85,6 +111,51 @@ sub run :Local :Args(0) {
 			$c->log->debug("Performing pg_dump.");
 			system qw(pg_dump -Fc -f), $fullname, '-Z', $self->compression,
 				$self->db_name;
+			$c->stash(size => -s $fullname);
+		});
+
+	$c->stash(ok => 1);
+}
+
+sub op_log_export : Private {
+	my ($self, $c, $device) = @_;
+	$c->stash(template => 'backup/op_log_export.tt2');
+
+	my $CSV = Text::CSV->new({
+			eol          => "\015\012",
+			always_quote => 1,
+			auto_diag    => 2,
+			diag_verbose => 1,
+		}) or die "Could not make CSV";
+
+	my $log = $c->model('Pool::Log')->search(undef, { order_by => 'log_num'})
+		or die "No log?";
+
+	my $now = DateTime->now;
+	my $filename = $now->strftime($self->export_relpath);
+	$c->stash(file => $filename);
+
+	$c->log->info("Requesting mount of $device...");
+	$c->model('DBus')->with_mount(
+		$device,
+		sub {
+			my $mount_path = shift;
+			$c->log->debug("Mounted at $mount_path.");
+			my $fullname = "$mount_path/$filename";
+
+			open my $fh, '>', $fullname
+				or die "Could not open $fullname: $!";
+			
+			my @cols = $log->result_source->columns;
+			$CSV->print($fh, \@cols);
+
+			while (my $rec = $log->next) {
+				$CSV->print($fh, [ map $rec->get_column($_), @cols ]);
+			}
+
+			close $fh
+				or die "Could not close $fullname: $!";
+			
 			$c->stash(size => -s $fullname);
 		});
 
